e950936159a1c527ffa28fe2b7673644d74c3ccd
RomanMillan <romandos962m@gmail.com>
2022-11-28 12:35:33 +0100

    paises

--- 1er Trimestre/Angular/paises/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 1er Trimestre/Angular/paises/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaisesServiceService } from '../../services/paises-service.service';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html'
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { 
+  constructor(private route: ActivatedRoute, private service:PaisesServiceService) { 
     console.log(route.snapshot.params['id']);
   }
 
   ngOnInit(): void {
+  }
+
+  get results(){
+    return this.service.results
   }
 
 }
--- 1er Trimestre/Angular/paises/src/app/pais/services/paises-service.service.ts
+++ 1er Trimestre/Angular/paises/src/app/pais/services/paises-service.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams} from '@angular/common/http';
+import { Restcountry } from '../interfaces/restconuntry';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesServiceService {
-  private api_key:string = 'lThbqpQEf9g3L2V8uSkAvyaDjzlf0b31';
-  private url:string = 'https://api.giphy.com/v1/gifs/search';
-  results:string[] = [];
-
+  private url:string = 'https://restcountries.com/v3.1/name/';
+  results:Restcountry[] = [];
   constructor(private http:HttpClient) {}
   
   searchCountry(query:string):void{
-    const params = new HttpParams()
-    .set('api_key', this.api_key)
-    .set('q', query)
-
-    this.http.get(this.url, {params})
-    /* .subscribe((resp)=> this.results =resp.toString) */
+    // results:Restcountry[] =  this.http.get<Restcountry[]>
+    this.http.get<Restcountry[]>(`${this.url}${query}`)
+    .subscribe((resp)=> this.results =resp) 
+    console.log(this.results)
   }
 
+  
 }

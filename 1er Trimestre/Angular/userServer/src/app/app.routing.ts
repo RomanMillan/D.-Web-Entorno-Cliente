96a06483666ad1bda51f6d72e52c673c41c8ddf0
RomanMillan <romandos962m@gmail.com>
2022-12-07 14:22:12 +0100

    trabajo userServer

---
+++ 1er Trimestre/Angular/userServer/src/app/app.routing.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users/users.component';
+import { ServersComponent } from './servers/servers/servers.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+
+const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'users',
+        component: UsersComponent
+    },
+    {
+        path:'servers',
+        component:ServersComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot( routes )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {}
---
+++ 1er Trimestre/Angular/userServer/src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterLink } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
+  exports:[
+    NavbarComponent,
+    NotFoundComponent,
+  ]
+})
+export class SharedModule { }
